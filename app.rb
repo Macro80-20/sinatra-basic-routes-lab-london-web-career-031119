c0f19c3f4e958685c66592662c324453d4d79d4a
Fernando <fernando_lopezc@live.com>
2019-04-02 07:22:53 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    #? User types in the URL 
+    get ('/name') {"My name is Fernando" } 
+    get ('/hometown') {"My hometown is London"}
+    get ('/favorite-song') {"My favorite song is numb"}       
+    
 end
